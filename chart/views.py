dd3a5e7a17f7d3d462d142d280a22e27606decc0
Marcos Carballal <mcarball@seas.upenn.edu>
2019-09-07 17:55:57 -0400

    Various additions to the django instance

--- chart/views.py
+++ chart/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render_to_response('index_t.html')
 # Create your views here.
