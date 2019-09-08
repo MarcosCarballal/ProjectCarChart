4a3b33e641a781f3e53085c3d378e2761a2dd190
Wishcle <cpmqaq@gmail.com>
2019-09-08 03:55:36 -0400

    drew axes

--- static/sketch.js
+++ static/sketch.js
@@ -1,23 +1,54 @@
 
-// var results = document.getElementsById('results').innerHTML;
 var results = decodeHtml(window.results);
+var cars = results.cars;
+var xLabel = results.xLabel;
+var yLabel = results.yLabel;
+
+var w, h;
+var o = 25;
 
 function setup() {
   var canvas = createCanvas(500, 500);
   canvas.parent('p5container');
   colorMode(HSB, 100);
   noLoop();
+  w = width;
+  h = height;
 }
 
 function draw() {
   background(60, 30, 100);
-  fill(0);
-  stroke(0,0,100);
+  drawGraph();
+}
 
+function drawGraph() {
+  drawAxes();
+  drawLabels();
+  plotCars();
 }
 
 function drawAxes() {
-  
+  push();
+  stroke(0);
+  strokeWeight(1);
+  line(o, h-o, o, o);
+  line(o, h-o, w-o, h-o);
+  pop();
+}
+
+function drawLabels() {
+  push();
+  translate(o/2, h-o/2);
+  textSize(14);
+  text("x axis", w/2.3, 5);
+  rotate(-PI / 2);
+  text("y axis", w/2.3, 5);
+  pop();
+}
+
+function plotCars() {
+  push();
+  pop();
 }
 
 function drawCar(car) {
